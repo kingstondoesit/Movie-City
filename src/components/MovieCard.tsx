9bb80e62c4dd12f112c9993a49f61b9ce983636f
kingstondoesit <kingstondoesitall@gmail.com>
2024-11-24 03:52:39 +0100

    fix: alt text visibility before movie poster load

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+
 // Define Moviecardprops and movie interface
 export interface MovieFeed {
   Title: string;
@@ -13,16 +15,24 @@ interface MovieCardProps {
 
 function MovieCard({ movie }: MovieCardProps) {
   const placeholderImage = 'https://via.placeholder.com/400';
+  const [isImageError, setIsImageError] = useState(false);
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   // Fallback image handler
   const handleImageError = (
     event: React.SyntheticEvent<HTMLImageElement, Event>
   ) => {
     event.currentTarget.src = placeholderImage;
+    setIsImageError(true); // Marks the image as failed
+  };
+
+  // Set image as loaded once it's successfully fetched
+  const handleImageLoad = () => {
+    setImageLoaded(true);
   };
 
   return (
-    <div className='movie'>
+    <div className="movie">
       <div>
         <p>{movie.Year}</p>
       </div>
@@ -30,24 +40,28 @@ function MovieCard({ movie }: MovieCardProps) {
       <div>
         <a
           href={`https://www.imdb.com/title/${movie.imdbID}`}
-          target='_blank'
-          rel='noopener noreferrer'
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <img
             src={movie.Poster !== 'N/A' ? movie.Poster : placeholderImage}
-            alt='Movie Poster'
-            onError={handleImageError} // Fallback logic
+            alt={isImageError ? `${movie.Title} Poster failed to load` : `${movie.Title} Poster`}
+            onError={handleImageError}
+            onLoad={handleImageLoad}
+            style={{
+              visibility: imageLoaded ? 'visible' : 'hidden', // Hide the alt text until the image is loaded
+            }}
           />
         </a>
       </div>
 
       <div>
-        <span>{movie.Type === 'series' ? 'Tv Show' : `${movie.Type}`}</span>
+        <span>{movie.Type === 'series' ? 'TV Show' : `${movie.Type}`}</span>
         <h3>
           <a
             href={`https://www.imdb.com/title/${movie.imdbID}`}
-            target='_blank'
-            rel='noopener noreferrer'
+            target="_blank"
+            rel="noopener noreferrer"
           >
             {`${movie.Title} (${movie.Year})`}
           </a>
